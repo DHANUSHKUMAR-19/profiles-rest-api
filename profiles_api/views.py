c0b2990b381f0c6a15281f3f55b31086e643b665
Dhanush <dhanushbedrabettu@gmail.com>
2024-08-03 22:25:37 +0530

    Added Get Put Post Delete Patch method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 
 # Create your views here.
 class HelloAPIView(APIView):
     """Test APIView."""
+    serializer_class=serializers.HelloSerializer
 
     def get(self,request,format=None):
         """Returns a list of APIView features"""
@@ -13,7 +16,38 @@ class HelloAPIView(APIView):
         'a',
         'b',
         'c',
-        'd'
+        'd',
+        'e'
         ]
 
         return Response({'message':'Hello','an_apiview':an_apiview})
+    def post(self,request):
+        serializer=self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            message=f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+    def put(self,request,pk=None):
+        """Handle updating an object"""
+        return Response({'method':'PUT'})
+
+    def patch(self,request,pk=None):
+        """Handling a partial object"""
+        return Response({'method':'PATCH'})
+    def delete(self,request,pk=None):
+        """Deleting an object"""
+        return Response({'method':'DELETE'})
+
+
+
+# for testing purpose
+# class Dhanush(APIView):
+#     """testing"""
+#     def get(self,request,format=None):
+#         a="apple"
+#         return  Response({'message':'test','a':a})
